caa8388d58cf0b1f11f22a180d9e81beda4dc17b
HongbiKo <rhghdql666@gmail.com>
2023-08-14 17:55:42 +0900

    Separate Components and learning about state (react day4)

--- src/App.js
+++ src/App.js
@@ -1,58 +1,36 @@
 import React, { Component } from "react";
+import TOC from "./components/TOC";
+import Subject from "./components/Subject";
+import Content from "./components/Content";
 import "./App.css";
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject_Subject: { title: "WEB", sub: "World Wide Web!" },
+      subject_Content: {
+        title: "HTML",
+        desc: "HTML is HyperText Markup Language.",
+      },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <Subject
+          title={this.state.subject_Subject.title}
+          sub={this.state.subject_Subject.sub}
+        ></Subject>
+        <TOC data={this.data.contents}></TOC>
         <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
+          title={this.state.subject_Content.title}
+          desc={this.state.subject_Content.desc}
         ></Content>
       </div>
     );
