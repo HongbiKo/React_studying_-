fb4b9289339342f10a97ccc42fd1f0b2fc1369ac
HongbiKo <rhghdql666@gmail.com>
2023-09-06 15:34:02 +0900

    CRUD - Delete implement (react day10)

--- src/App.js
+++ src/App.js
@@ -118,7 +118,21 @@ class App extends Component {
         ></TOC>
         <Control
           onChangeMode={function (_mode) {
-            this.setState({ mode: _mode });
+            if (_mode === "delete") {
+              if (window.confirm("really?")) {
+                const _contents = Array.from(this.state.contents);
+                for (let i = 0; i < _contents.length; i++) {
+                  if (_contents[i].id === this.state.selected_content_id) {
+                    _contents.splice(i, 1);
+                    break;
+                  }
+                }
+                this.setState({ contents: _contents, mode: "welcome" });
+                alert("deleted!");
+              }
+            } else {
+              this.setState({ mode: _mode });
+            }
           }.bind(this)}
         ></Control>
         {this.getContent()}
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 class TOC extends Component {
   shouldComponentUpdate(newProps, newState) {
     console.log("==> TOC render shouldComponentUpdate");
-    // console.log(newProps.data, this.props.data);
     if (this.props.data === newProps.data) {
       return false;
     }
