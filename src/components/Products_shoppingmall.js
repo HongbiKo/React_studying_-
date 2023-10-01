c9247399124481948bd9d5ebf8c91a5994b0ee1d
HongbiKo <rhghdql666@gmail.com>
2023-10-01 16:43:32 +0900

    practice path parameter 2

--- src/components/Products_shoppingmall.js
+++ src/components/Products_shoppingmall.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Routes, Route, useParams, Link } from "react-router-dom";
 
 function Products_shoppingmall() {
   let params = useParams();
-  const product_id = params.product_id;
   console.log(params);
+
   return (
     <div>
       <h1>Products 페이지</h1>
-      <div>{product_id}번 상품</div>
+
+      <Link to="/products/1">1</Link>
+      <br></br>
+      <Link to="/products/2">2</Link>
+
+      <Routes>
+        <Route path="/*" element={<div>상품을 선택해주세요.</div>}></Route>
+
+        <Route path=":product_id" element={<Product />} />
+      </Routes>
     </div>
   );
+}
+
+function Product() {
+  let params = useParams();
+  const product_id = params.product_id;
+  console.log(product_id);
+  return <div>{product_id}번 상품</div>;
 }
 
 export default Products_shoppingmall;
